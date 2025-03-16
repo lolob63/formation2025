a5d9b3c2189f6aa95942ab32b36e0c8061665b74
Laurent <laurent.barthomeuf@ac-clermont.fr>
2025-03-16 11:32:53 +0100

    xx

--- esp32c6/esp32c6imuAPI/esp32c6imuAPI.ino
+++ esp32c6/esp32c6imuAPI/esp32c6imuAPI.ino
@@ -4,9 +4,15 @@ Carte XIAO ESP32C6 + Shield
 
 API REST 
 curl GET 'http://192.168.1.76/data'
-
+curl GET 'http://192.168.1.76/aX'
+curl GET 'http://192.168.1.76/aY'
+curl GET 'http://192.168.1.76/aZ'
+curl --location 'http://192.168.1.76/config' \
+     --header 'Content-Type: application/json' \
+     --data '{"config":"ip"}'
+https://wiki.seeedstudio.com/xiao_esp32c6_getting_started/ 
 https://wiki.seeedstudio.com/Grove-Shield-for-Seeeduino-XIAO-embedded-battery-management-chip/
-https://wiki.seeedstudio.com/Seeeduino-XIAO-Expansion-Board/
+https://wiki.seeedstudio.com/Seeeduino-XIAO-Expansion-Board/ 
 https://wiki.seeedstudio.com/Grove-3-Axis_Digital_Accelerometer-16g/
 https://docs.espressif.com/projects/esp-idf/en/v4.2/esp32s2/api-reference/system/freertos.html#
 
@@ -44,15 +50,7 @@ ADXL345 adxl;  //instancie l'objet adxl à partir de la librairie ADXL345
 double ax, ay, az;
 int x, y, z;
 
-void setup_routing() {     
-  server.on("/aX", getaX);     
-  server.on("/aY", getaY);     
-  server.on("/aZ", getaZ); 
-  server.on("/data", getData);    
-  //server.on("/led", HTTP_POST, handlePost);    
-          
-  server.begin();    
-}
+
  
 void create_json(char *tag, float value, char *unit) {  
   jsonDocument.clear();  
@@ -68,12 +66,12 @@ void read_sensor_data(void * parameter) {
     
     adxl.readXYZ(&x, &y, &z);  //read the accelerometer values and store them in variables  x,y,z
     // Output x,y,z values
-    Serial.print("values of X , Y , Z: ");
-    Serial.print(x);
-    Serial.print(" , ");
-    Serial.print(y);
-    Serial.print(" , ");
-    Serial.println(z);
+    //Serial.print("values of X , Y , Z: ");
+    //Serial.print(x);
+    //Serial.print(" , ");
+    //Serial.print(y);
+    //Serial.print(" , ");
+    //Serial.println(z);
 
     double xyz[3];
 
@@ -81,16 +79,16 @@ void read_sensor_data(void * parameter) {
     ax = xyz[0];
     ay = xyz[1];
     az = xyz[2];
-    Serial.print("X=");
-    Serial.print(ax);
-    Serial.println(" g");
-    Serial.print("Y=");
-    Serial.print(ay);
-    Serial.println(" g");
-    Serial.print("Z=");
-    Serial.print(az);
-    Serial.println(" g");
-    Serial.println("**********************");
+    //Serial.print("X=");
+    //Serial.print(ax);
+    //Serial.println(" g");
+    //Serial.print("Y=");
+    //Serial.print(ay);
+    //Serial.println(" g");
+    //Serial.print("Z=");
+    //Serial.print(az);
+    //Serial.println(" g");
+    //Serial.println("**********************");
     String AX = "ax = " + String(ax*9.8);
     String AY = "ax = " + String(ay*9.8);
     String AZ = "ay = " + String(az*9.8);
@@ -112,6 +110,25 @@ void afficheHeureDate() {
   Serial.println("");
   Serial.print(rtc.getLocalEpoch());
   Serial.println("");
+}
+void handlePost() {
+  if (server.hasArg("plain") == false) {
+  }
+  String body = server.arg("plain");
+  deserializeJson(jsonDocument, body);
+
+  String valConfig = jsonDocument["config"];
+
+  // envoie sur la valeur sur la LED.
+  if(valConfig == "ip"){
+    Serial.print("test de commande adresse ip  ");
+    Serial.println();
+  }
+  if(valConfig == "unit"){
+    Serial.print("test de commande unit   ");
+    Serial.println();
+  }
+  server.send(200, "application/json", "{}");
 }
 void getaX() {
   //Serial.println("Lecture de la temperature");
@@ -144,6 +161,16 @@ void add_json_object(char *tag, float value, char *unit) {
   obj["type"] = tag;
   obj["valeur"] = value;
   obj["unite"] = unit; 
+}
+
+void setup_routing() {     
+  server.on("/aX", getaX);     
+  server.on("/aY", getaY);     
+  server.on("/aZ", getaZ); 
+  server.on("/data", getData);    
+  server.on("/config", HTTP_POST, handlePost);    
+          
+  server.begin();    
 }
 
 void add_json_object2(char *tag, unsigned long value, char *unit) {
